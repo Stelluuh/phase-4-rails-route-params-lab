b1efa0566cf341980e18ee8e61d6d50415451d39
stella <sftamesis13@gmail.com>
2023-05-15 11:52:28 -0700

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,11 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name] #if the :name paramenter is present in the hash
+      Student.by_name(params[:name]) # .by_name is in models file.
+    else
+      Student.all
+    end
     render json: students
   end
 
